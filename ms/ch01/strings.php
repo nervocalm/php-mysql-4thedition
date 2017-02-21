11b572415ef5b9eaf5706bdf04cc9dc6dbfcb8b8
Johannes Kepler <nervocalm@outlook.com>
2017-02-21 16:00:11 -0300

    Standard strings.php example

---
+++ ms/ch01/strings.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Strings</title>
+	</head>
+	
+	<body>
+		<?php 
+			$first_name = "Johannes";
+			$last_name = "Kepler";
+			$book_name = "Astronomia Nova";
+
+			echo "<p>The book <strong>$book_name</strong> was written by <em>$first_name $last_name</em> </p>";
+		 ?>
+
+	</body>
+
+</html>
