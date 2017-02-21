50f8f376a1c933b9cf1d729e2ffbd89e915ebeab
Johannes Kepler <nervocalm@outlook.com>
2017-02-21 00:59:31 -0300

    Base test files for local Apache.

---
+++ phpinfo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="us"
+	<head>
+		<title>This is a Test PHP page</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	</head>
+
+	<body>
+		<header>
+			<h1>Welcome!</h1>
+		</header>
+		<?php
+			printf("<h3> Mother Fucker! </h3>");
+		?>
+		<section>
+		</section>
+
+		<footer>
+			<small>All Right Reserved to Hell.Org</small>
+		</footer>
+	</body>
+</html>
+
